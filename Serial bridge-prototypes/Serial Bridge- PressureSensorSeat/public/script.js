63107978415ae3a94bc497639c8ccb564c8c8591
Malin <malin.benson@gmail.com>
2019-03-26 13:50:27 +0100

    Comments Added

--- Serial bridge-prototypes/Serial Bridge- PressureSensorSeat/public/script.js
+++ Serial bridge-prototypes/Serial Bridge- PressureSensorSeat/public/script.js
@@ -3,9 +3,12 @@ var startstartll;
 if (document.readyState != 'loading') onDocumentReady();
 else document.addEventListener('DOMContentLoaded', onDocumentReady);
 
+/*Group changed function to: 
+1. Augment text displayed on localhost to show data from pressure sensor on the Arduino
+2. If value from pressure sensor is over 500 start a function, otherwise it will cal for another (to stop timer)*/
 function handleCommand(d) {
   lastMsgEl.innerHTML =  "sensor value is:" + d.float;
-
+ 
   if (d.float > 500) {
     printDuration();
   }
@@ -56,14 +59,18 @@ function onDocumentReady() {
     })
 }
 
+/*We declared variables for time-keeping of sensor input, "start" to start the counter, and "time" to store 
+the time it was held*/
+
 var start = null;
 var time = 0;
 
+/*We created function to be called whn sensor- data is over 500. This will start adding 1 second to 
+a "counter" as long as the sensor stays pressed to keep track of time pressed*/ 
 function printDuration() {
   if (start == null) {
     time = 0;
-    document.body.style.backgroundColor = 'white';
-
+    document.body.style.backgroundColor = "white";
     start = setInterval(function () {
       time += 1;
       document.getElementById('sendForm').innerHTML = time;
@@ -71,6 +78,11 @@ function printDuration() {
   }
 }
 
+/*This clears the time variable when pressure is released and value drops, stopping the timer.
+After the pressure is released the function will check if the time it (FSR sensor) was pressed
+is over or under 20 seconds. If over 20 seconds the background turns green, if under it turns red -> indicating 
+more sitting / exercise is needed. The start variable is also cleared, allowing the functions to start over 
+when applying pressure again */
 function stop() {
   clearInterval(start);
   start = null;
@@ -80,6 +92,5 @@ function stop() {
   }
   else {
     document.body.style.backgroundColor = 'red';
-
   }
 }
