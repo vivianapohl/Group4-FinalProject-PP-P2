bc3a0180d081c665a6df82e2abe14c51cb10b8c9
LeftoverKompot <leftover.kompot@gmail.com>
2019-03-27 01:27:00 +0100

    commit

--- Serial bridge-prototypes/serial motion mashup light switch 1st iteration/arduino/LightSwitch.ino
+++ Serial bridge-prototypes/serial motion mashup light switch 1st iteration/arduino/LightSwitch.ino
@@ -1,9 +1,10 @@
+/*Simple arduino code witch mostly works on serial.write and serial read function to monitor my actions.*/
 int ledPin = 2;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
-  Serial.println("Enter On to turn on the LED!");
+  Serial.println("Press On to turn on the LED!");
 }
 
 void loop(){
@@ -12,12 +13,12 @@ void loop(){
     if (ch == 'a'){
       digitalWrite(ledPin, HIGH);
       Serial.println("You have turned on the LED!");
-      Serial.println("Enter Off to turn off the LED!");
+      Serial.println("Press Off to turn off the LED!");
     }
     if (ch == 'b'){
       digitalWrite(ledPin, LOW);
       Serial.println("You have turned off the LED!");
-      Serial.println("Enter On to turn on the LED!");
+      Serial.println("Press On to turn on the LED!");
     }
   }
 }
